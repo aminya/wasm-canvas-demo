7a63b54df510a40556eeef78110ba1131fcdbece
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-17 21:21:11 -0600

    Remove screen

--- graphics.cpp
+++ graphics.cpp
@@ -5,7 +5,6 @@ const unsigned int HEIGHT = 600;
 unsigned int BUFFER[WIDTH * HEIGHT];
 
 void draw() {
-    unsigned int screen[600][600];
     for (int x = 0; x < WIDTH; x++) {
         for (int y = 0; y < HEIGHT; y++) {
 
@@ -16,8 +15,7 @@ void draw() {
                 color = 0xffff0000; // RGBA little endian
             }
 
-            screen[x][y] = color;
-            BUFFER[WIDTH * x + y] = screen[x][y];
+            BUFFER[WIDTH * x + y] = color;
         }
     }
 }
--- graphics.d
+++ graphics.d
@@ -5,7 +5,6 @@ immutable uint HEIGHT = 600;
 uint[WIDTH * HEIGHT] BUFFER;
 
 void draw() @safe @nogc {
-    uint[600][600] screen;
     for (int x = 0; x < WIDTH; x++) {
         for (int y = 0; y < HEIGHT; y++) {
 
@@ -16,8 +15,7 @@ void draw() @safe @nogc {
                 color = 0xffff0000; // RGBA little endian
             }
 
-            screen[x][y] = color;
-            BUFFER[WIDTH * x + y] = screen[x][y];
+            BUFFER[WIDTH * x + y] = color;
         }
     }
 }
